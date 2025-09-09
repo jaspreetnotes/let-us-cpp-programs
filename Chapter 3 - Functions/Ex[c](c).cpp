fcc504174b65d3a96cd4c98ccf1e2afc352010dd
Jaspreet Singh <nesonotes@gmail.com>
2025-09-09 16:18:43 +0530

    Added program of Exercise [c](a)

---
+++ Chapter 3 - Functions/Ex[c](c).cpp
@@ -0,0 +1,26 @@
+/* Write a program that define a function display() whose prototype is given below:
+   void display(char ch = '*', int num = 80);
+   Call this function to display the lines shown below:
+   *****
+   ---------------
+   =====================
+*/
+
+#include <iostream>
+using namespace std;
+
+void display(char ch = '*', int num = 80);
+int main()
+{
+    display('*', 5);
+    display('-', 15);
+    display('=', 20);
+    return 0;
+}
+
+void display(char ch, int num) {
+    for (int i = 0; i < num; i++) {
+        cout << ch;
+    }
+    cout << "\n";
+}
